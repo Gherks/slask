0f9fd531bfab237c6195a7727a05304664bb4e6b
Per <perjohanhellstrom@gmail.com>
2019-02-11 21:28:50 +0100

    added ef and api

---
+++ Slask.Data/Models/Tournament/TournamentModel.cs
@@ -0,0 +1,17 @@
+﻿using Slask.Data.Models.Tournament;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slask.Data.Models
+{
+    public class TournamentModel
+    {
+        public Guid Id { get; set; }
+        public IList<RoundModel> Rounds { get; set; }
+        public IList<PlayerModel> Players { get; set; }
+        public IList<BetterModel> Betters { get; set; }
+        public IList<SettingsModel> Settings { get; set; }
+    }
+}
