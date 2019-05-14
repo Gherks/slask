6dff03f8fe326ef5b9ebeefd9c7a45251a25d560
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2019-05-14 21:04:28 +0200

    Add DBLogger projects

---
+++ DBLogger.Domain/Log.cs
@@ -0,0 +1,9 @@
+﻿namespace DBLogger.Domain
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public byte Severity { get; set; }
+    }
+}
---
+++ DBLogger.Persistance/DBLoggerContext.cs
@@ -0,0 +1,24 @@
+﻿using DBLogger.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBLogger.Persistance
+{
+    public class DBLoggerContext : DbContext
+    {
+        public DBLoggerContext()
+        {
+        }
+
+        public DBLoggerContext(DbContextOptions<DBLoggerContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Database = SlaskDB; Trusted_Connection = True;");
+        }
+
+        public DbSet<Log> Logs { get; set; }
+    }
+}
