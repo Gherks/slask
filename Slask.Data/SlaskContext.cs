ef3ad0b56f32de9a2b63c401f8b178b3ef7b9e79
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2019-06-04 20:45:15 +0200

    Remove OnModelCreating from SlaskContext

--- Slask.Data/SlaskContext.cs
+++ Slask.Data/SlaskContext.cs
@@ -19,23 +19,6 @@ namespace Slask.Data
             optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Database = SlaskDB; Trusted_Connection = True;");
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Player>().HasData(
-                new Player { Id = 1, Name = "Taeja" },
-                new Player { Id = 2, Name = "Rain" },
-                new Player { Id = 3, Name = "Bomber" },
-                new Player { Id = 4, Name = "FanTaSy" },
-                new Player { Id = 5, Name = "Stephano" },
-                new Player { Id = 6, Name = "Thorzain" },
-                new Player { Id = 7, Name = "Crank" },
-                new Player { Id = 8, Name = "DeMuslim" },
-                new Player { Id = 9, Name = "Stats" },
-                new Player { Id = 10, Name = "ZerO" },
-                new Player { Id = 11, Name = "Stork" }
-                );
-        }
-
         public DbSet<Tournament> Tournaments { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Player> Players { get; set; }
