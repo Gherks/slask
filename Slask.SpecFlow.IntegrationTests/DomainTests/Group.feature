9af14ee5fcf700175414db3b467d9ce9c1979121
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2019-11-13 22:35:36 +0100

    Add first version of Group Specflow tests

---
+++ Slask.SpecFlow.IntegrationTests/DomainTests/Group.feature
@@ -0,0 +1,33 @@
+﻿Feature: Group
+	Does a bunch of tests on Group
+
+@GroupTag
+Scenario: Rounds can create groups
+	Given a tournament creates rounds
+		| Round type      | Round name            | Best of | Advancing amount |
+		| Bracket         | Bracket round         | 3       | 1                |
+		| Dual tournament | Dual tournament round | 3       | 1                |
+		| Round robin     | Round robin round     | 3       | 1                |
+	When created rounds 0 to 2 creates 1 groups each
+	Then created rounds 0 to 2 should contain 1 groups each
+
+Scenario: Adding group to bracket round creates bracket group
+	Given a tournament creates rounds
+		| Round type | Round name    | Best of | Advancing amount |
+		| Bracket    | Bracket round | 3       | 1                |
+	When group is added to created round 0
+	Then group 0 should be valid of type "Bracket"
+
+Scenario: Adding group to dual tournament round creates bracket group
+	Given a tournament creates rounds
+		| Round type      | Round name            | Best of | Advancing amount |
+		| Dual tournament | Dual tournament round | 3       | 1                |
+	When group is added to created round 0
+	Then group 0 should be valid of type "Dual tournament"
+
+Scenario: Adding group to round robin round creates bracket group
+	Given a tournament creates rounds
+		| Round type  | Round name        | Best of | Advancing amount |
+		| Round robin | Round robin round | 3       | 0                |
+	When group is added to created round 0
+	Then group 0 should be valid of type "Round robin"
---
+++ Slask.SpecFlow.IntegrationTests/DomainTests/GroupSteps.cs
@@ -0,0 +1,87 @@
+﻿using FluentAssertions;
+using Slask.Domain;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace Slask.SpecFlow.IntegrationTests.DomainTests
+{
+    [Binding, Scope(Feature = "Group")]
+    public class GroupSteps : GroupStepDefinitions
+    {
+
+    }
+
+    public class GroupStepDefinitions : RoundStepDefinitions
+    {
+        protected readonly List<GroupBase> createdGroups;
+
+        public GroupStepDefinitions()
+        {
+            createdGroups = new List<GroupBase>();
+        }
+
+        [When(@"created rounds (.*) to (.*) creates (.*) groups each")]
+        public void WhenCreatedRoundsToCreatesGroupsEach(int roundStartIndex, int roundEndIndex, int groupAmount)
+        {
+            for (int roundIndex = roundStartIndex; roundIndex < roundEndIndex; ++roundIndex)
+            {
+                for (int groupCounter = 0; groupCounter < groupAmount; ++groupCounter)
+                {
+                    createdRounds[roundIndex].AddGroup();
+                }
+            }
+        }
+
+        [When(@"group is added to created round (.*)")]
+        public void WhenGroupIsAddedToCreatedRound(int roundIndex)
+        {
+            createdGroups.Add(createdRounds[roundIndex].AddGroup());
+        }
+
+        [Then(@"created rounds (.*) to (.*) should contain (.*) groups each")]
+        public void ThenCreatedRoundsToShouldContainGroupsEach(int roundStartIndex, int roundEndIndex, int groupAmount)
+        {
+            for (int roundIndex = roundStartIndex; roundIndex < roundEndIndex; ++roundIndex)
+            {
+                for (int groupCounter = 0; groupCounter < groupAmount; ++groupCounter)
+                {
+                    createdRounds[roundIndex].Groups.Should().HaveCount(groupAmount);
+                }
+            }
+        }
+
+        [Then(@"group (.*) should be valid of type ""(.*)""")]
+        public void ThenGroupShouldBeOfType(int groupIndex, string roundType)
+        {
+            if(ParseRoundType(roundType, out RoundType type))
+            {
+                GroupBase group = createdGroups[groupIndex];
+
+                if (type == RoundType.Bracket)
+                {
+                    CheckRoundValidity<BracketGroup>(group);
+                }
+                else if (type == RoundType.DualTournament)
+                {
+                    CheckRoundValidity<DualTournamentGroup>(group);
+                }
+                else if (type == RoundType.RoundRobin)
+                {
+                    CheckRoundValidity<RoundRobinGroup>(group);
+                }
+            }
+        }
+
+        protected static void CheckRoundValidity<GroupType>(GroupBase group)
+        {
+            group.Should().NotBeNull();
+            group.Should().BeOfType<GroupType>();
+            group.Id.Should().NotBeEmpty();
+            group.ParticipatingPlayers.Should().BeEmpty();
+            group.Matches.Should().BeEmpty();
+            group.RoundId.Should().NotBeEmpty();
+            group.Round.Should().NotBeNull();
+        }
+    }
+}
