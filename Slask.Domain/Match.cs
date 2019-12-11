dcdc36fbbeec7e0e362678ec5e27182d4a068ba4
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2019-12-11 21:05:10 +0100

    Add some notes for future head scratching

--- Slask.Domain/Match.cs
+++ Slask.Domain/Match.cs
@@ -57,6 +57,7 @@ namespace Slask.Domain
             return Player1.PlayerReference != null && Player2.PlayerReference != null;
         }
 
+        // Needs to restricted to GroupBase?
         public bool AddPlayerReference(PlayerReference playerReference)
         {
             if (playerReference == null)
@@ -79,6 +80,7 @@ namespace Slask.Domain
             return false;
         }
 
+        // Needs to restricted to GroupBase?
         public bool AssignPlayerReferences(PlayerReference player1Reference, PlayerReference player2Reference)
         {
             if (player1Reference == null || player2Reference == null || player1Reference.Id != player2Reference.Id)
