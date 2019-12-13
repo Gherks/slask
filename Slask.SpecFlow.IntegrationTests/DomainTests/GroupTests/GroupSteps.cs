b295f2c356abd9fa4ff4d3385214d016cab17c58
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2019-12-13 17:35:33 +0100

    Make Specflow step available to other group tests

--- Slask.SpecFlow.IntegrationTests/DomainTests/GroupTests/GroupSteps.cs
+++ Slask.SpecFlow.IntegrationTests/DomainTests/GroupTests/GroupSteps.cs
@@ -231,6 +231,25 @@ namespace Slask.SpecFlow.IntegrationTests.DomainTests.GroupTests
             }
         }
 
+
+        [Then(@"pariticpating players in created group (.*) should be mapped accordingly")]
+        public void ThenPariticpatingPlayersInCreatedGroupShouldBeMappedAccordingly(int createdGroupIndex, Table table)
+        {
+            GroupBase group = createdGroups[createdGroupIndex];
+
+            foreach (TableRow row in table.Rows)
+            {
+                row["Match index"].Should().NotBeNullOrEmpty();
+                row["Player 1 name"].Should().NotBeNull();
+                row["Player 2 name"].Should().NotBeNull();
+
+                ParseBracketGroupMatchSetup(row, out int matchIndex, out string player1Name, out string player2Name);
+
+                group.Matches[matchIndex].Player1.Name.Should().Be(player1Name);
+                group.Matches[matchIndex].Player2.Name.Should().Be(player2Name);
+            }
+        }
+
         protected static void CheckGroupValidity<GroupType>(GroupBase group, int matchesUponCreation)
         {
             group.Should().NotBeNull();
@@ -261,6 +280,30 @@ namespace Slask.SpecFlow.IntegrationTests.DomainTests.GroupTests
             if (row.ContainsKey("Group index"))
             {
                 int.TryParse(row["Group index"], out groupIndex);
+            }
+        }
+
+        protected static void ParseBracketGroupMatchSetup(TableRow row, out int matchIndex, out string player1Name, out string player2Name)
+        {
+            matchIndex = -1;
+            player1Name = "";
+            player2Name = "";
+
+            if (row.ContainsKey("Match index"))
+            {
+                int.TryParse(row["Match index"], out matchIndex);
+            }
+
+            matchIndex.Should().BeGreaterOrEqualTo(0);
+
+            if (row.ContainsKey("Player 1 name"))
+            {
+                player1Name = row["Player 1 name"];
+            }
+
+            if (row.ContainsKey("Player 2 name"))
+            {
+                player2Name = row["Player 2 name"];
             }
         }
 
