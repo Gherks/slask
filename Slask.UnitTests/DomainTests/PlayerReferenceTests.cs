bbac050723f30822507400d557cf9ad76413c29e
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2019-12-14 13:22:59 +0100

    Rectify and extend PlayerReference unit tests

--- Slask.UnitTests/DomainTests/PlayerReferenceTests.cs
+++ Slask.UnitTests/DomainTests/PlayerReferenceTests.cs
@@ -11,23 +11,38 @@ namespace Slask.UnitTests.DomainTests
         [Fact]
         public void CanCreatePlayerReference()
         {
-            TournamentServiceContext services = GivenServices();
-            RoundRobinGroup group = HomestoryCupSetup.Part05AddedPlayersToRoundRobinGroup(services);
-            PlayerReference playerReference = group.Matches.First().Player1.PlayerReference;
+            Tournament tournament = Tournament.Create("GSL 2019");
+            PlayerReference playerReference = PlayerReference.Create("Maru", tournament);
 
             playerReference.Should().NotBeNull();
             playerReference.Id.Should().NotBeEmpty();
             playerReference.Name.Should().Be("Maru");
-            playerReference.TournamentId.Should().Be(group.Round.Tournament.Id);
-            playerReference.Tournament.Should().Be(group.Round.Tournament);
+            playerReference.TournamentId.Should().Be(tournament.Id);
+            playerReference.Tournament.Should().Be(tournament);
+        }
+
+        [Fact]
+        public void CannotCreatePlayerReferenceWithEmptyname()
+        {
+            Tournament tournament = Tournament.Create("GSL 2019");
+            PlayerReference playerReference = PlayerReference.Create("", tournament);
+
+            playerReference.Should().BeNull();
+        }
+
+        [Fact]
+        public void CannotCreatePlayerReferenceWithNullTournament()
+        {
+            PlayerReference playerReference = PlayerReference.Create("Maru", null);
+
+            playerReference.Should().BeNull();
         }
 
         [Fact]
         public void PlayerReferenceCanBeRenamed()
         {
-            TournamentServiceContext services = GivenServices();
-            RoundRobinGroup group = HomestoryCupSetup.Part05AddedPlayersToRoundRobinGroup(services);
-            PlayerReference playerReference = group.Matches.First().Player1.PlayerReference;
+            Tournament tournament = Tournament.Create("GSL 2019");
+            PlayerReference playerReference = PlayerReference.Create("Maru", tournament);
 
             playerReference.RenameTo("Idra");
 
@@ -37,30 +52,27 @@ namespace Slask.UnitTests.DomainTests
         [Fact]
         public void PlayerReferenceCannotBeRenamedToSameAsOtherPlayerReferenceNoMatterLetterCasing()
         {
-            TournamentServiceContext services = GivenServices();
-            RoundRobinGroup group = HomestoryCupSetup.Part05AddedPlayersToRoundRobinGroup(services);
-            Match match = group.Matches.First();
+            string firstName = "Maru";
+            string secondName = "Idra";
 
-            match.Player1.PlayerReference.RenameTo(match.Player2.Name.ToUpper());
+            Tournament tournament = Tournament.Create("GSL 2019");
+            PlayerReference maruPlayerReference = PlayerReference.Create(firstName, tournament);
+            PlayerReference idraPlayerReference = PlayerReference.Create(secondName, tournament);
 
-            match.Player1.Name.Should().Be("Maru");
+            idraPlayerReference.RenameTo(maruPlayerReference.Name.ToUpper());
+
+            idraPlayerReference.Name.Should().Be(secondName);
         }
 
         [Fact]
         public void PlayerReferenceCannotBeRenamedToEmptyName()
         {
-            TournamentServiceContext services = GivenServices();
-            RoundRobinGroup group = HomestoryCupSetup.Part05AddedPlayersToRoundRobinGroup(services);
-            PlayerReference playerReference = group.Matches.First().Player1.PlayerReference;
+            Tournament tournament = Tournament.Create("GSL 2019");
+            PlayerReference playerReference = PlayerReference.Create("Maru", tournament);
 
             playerReference.RenameTo("");
 
             playerReference.Name.Should().Be("Maru");
-        }
-
-        private TournamentServiceContext GivenServices()
-        {
-            return TournamentServiceContext.GivenServices(new UnitTestSlaskContextCreator());
         }
     }
 }
