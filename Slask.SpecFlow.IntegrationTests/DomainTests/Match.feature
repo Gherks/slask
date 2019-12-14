7a01537d5f45ed92e3ccf4bc3ffd80767b3dba24
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2019-12-14 12:32:39 +0100

    Create PlayState Specflow tests for matches

--- Slask.SpecFlow.IntegrationTests/DomainTests/Match.feature
+++ Slask.SpecFlow.IntegrationTests/DomainTests/Match.feature
@@ -2,13 +2,23 @@
 	Does a bunch of tests on Matches
 
 @MatchTag
-#Scenario: Match returns NotBegun state before match is played
+Scenario: Match returns NotBegun state before match is played
+	When a round robin tournament with users and players has been created
+	Then match 0 in created group 0 should be in state "NotBegun"
 
+Scenario: Match returns IsPlaying state when it has started but not finished
+	Given a round robin tournament with users and players has been created
+	When score is added to players in given matches in created groups
+		| Created group index | Match index | Scoring player | Added score |
+		| 0                   | 0           | First          | 1           |
+	Then match 0 in created group 0 should be in state "IsPlaying"
 
-#Scenario: Match returns IsPlaying state when it has started but not finished
-
-
-#Scenario: Match returns IsFinished state when match is played out
+Scenario: Match returns IsFinished state when match is played out
+	Given a round robin tournament with users and players has been created
+		And groups within created tournament is played out and betted on
+			| Created tournament index | Round index | Group index |
+			| 0                        | 0           | 0           |
+	Then match 0 in created group 0 should be in state "IsFinished"
 
 
 #Scenario: Can return winning player when match is finished
---
+++ Slask.SpecFlow.IntegrationTests/DomainTests/MatchSteps.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using Slask.Domain;
+using Slask.SpecFlow.IntegrationTests.DomainTests.GroupTests;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace Slask.SpecFlow.IntegrationTests.DomainTests
+{
+    [Binding, Scope(Feature = "Match")]
+    public class MatchSteps : MatchStepDefinitions
+    {
+
+    }
+
+    public class MatchStepDefinitions : RoundRobinGroupStepDefinitions
+    {
+        [Given(@"a round robin tournament with users and players has been created")]
+        [When(@"a round robin tournament with users and players has been created")]
+        public void GivenARoundRobinTournamentWithUsersAndPlayersHasBeenCreated()
+        {
+            GivenATournamentNamedWithUsersAddedToIt("GSL 2019", "Stålberto, Bönis, Guggelito");
+
+            Table roundTable = new Table(new string[] {
+                            "Round type",
+                            "Round name",
+                            "Best of",
+                            "Advancing amount"});
+
+            roundTable.AddRow(new string[] {
+                            "Round robin",
+                            "Round robin round",
+                            "3",
+                            "3"});
+
+            GivenCreatedTournamentAddsRounds(0, roundTable);
+            GivenGroupIsAddedToCreatedRound(0);
+            GivenPlayersIsAddedToCreatedGroup("First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth", 0);
+        }
+
+        [Then(@"match (.*) in created group (.*) should be in state ""(.*)""")]
+        public void ThenCreatedMatchInCreatedGroupShouldBeInState(int matchIndex, int createdGroupIndex, string playStateString)
+        {
+            PlayState playState = GetPlayStateFromString(playStateString);
+
+            GroupBase group = createdGroups[createdGroupIndex];
+            Match match = group.Matches[matchIndex];
+
+            match.GetPlayState().Should().Be(playState);
+        }
+
+        private PlayState GetPlayStateFromString(string playStateString)
+        {
+            if (playStateString == null)
+            {
+                throw new ArgumentNullException(nameof(playStateString));
+            }
+
+            if (playStateString == "NotBegun")
+            {
+                return PlayState.NotBegun;
+            }
+            else if(playStateString == "IsPlaying")
+            {
+                return PlayState.IsPlaying;
+            }
+            else if(playStateString == "IsFinished")
+            {
+                return PlayState.IsFinished;
+            }
+            else
+            {
+                throw new Exception("Invalid playStateString given, could not map it to existing play states");
+            }
+        }
+    }
+}
