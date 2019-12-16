594d9c1bfd1cceb0083277ce4418a27f72921513
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2019-12-16 21:15:07 +0100

    Improve unit tests for Player object
    Create tests for the different kinds of rounds a player can belong to.

---
+++ Slask.UnitTests/DomainTests/PlayerTests/PlayerInRoundRobinGroupTests.cs
@@ -0,0 +1,100 @@
+﻿using FluentAssertions;
+using Slask.Common;
+using Slask.Domain;
+using Slask.Domain.Rounds;
+using System.Linq;
+using Xunit;
+
+namespace Slask.UnitTests.DomainTests
+{
+    public class PlayerInRoundRobinGroupTests
+    {
+        private Tournament tournament;
+        private RoundRobinRound round;
+        private RoundRobinGroup group;
+        private PlayerReference playerReference;
+        private Match match;
+
+        public PlayerInRoundRobinGroupTests()
+        {
+            tournament = Tournament.Create("GSL 2019");
+            round = tournament.AddRoundRobinRound("Bracket round", 7, 1) as RoundRobinRound;
+            group = round.AddGroup() as RoundRobinGroup;
+            playerReference = group.AddPlayerReference("Maru");
+            group.AddPlayerReference("Stork");
+            match = group.Matches.First();
+        }
+
+        [Fact]
+        public void CanCreatePlayer()
+        {
+            match.Player1.Should().NotBeNull();
+            match.Player1.Id.Should().NotBeEmpty();
+            match.Player1.PlayerReference.Should().Be(playerReference);
+            match.Player1.Score.Should().Be(0);
+            match.Player1.MatchId.Should().Be(match.Id);
+            match.Player1.Match.Should().Be(match);
+        }
+
+        [Fact]
+        public void CanAssignPlayerReference()
+        {
+            PlayerReference playerReference = PlayerReference.Create("Taeja", tournament);
+            match.Player1.SetPlayerReference(playerReference);
+
+            match.Player1.PlayerReference.Should().Be(playerReference);
+        }
+
+        [Fact]
+        public void CanChangePlayerReferenceToAnotherPlayerReference()
+        {
+            PlayerReference playerReference = PlayerReference.Create("Taeja", tournament);
+            match.Player1.SetPlayerReference(playerReference);
+
+            match.Player1.PlayerReference.Should().Be(playerReference);
+        }
+
+        [Fact]
+        public void CanSetExistingPlayerReferenceToNull()
+        {
+            PlayerReference playerReference = PlayerReference.Create("Taeja", tournament);
+
+            match.Player1.SetPlayerReference(playerReference);
+            match.Player1.SetPlayerReference(null);
+
+            match.Player1.PlayerReference.Should().BeNull();
+        }
+
+        [Fact]
+        public void CanIncreaseScore()
+        {
+            SystemTimeMocker.Set(match.StartDateTime.AddMinutes(1));
+            int score = 1;
+
+            match.Player1.IncreaseScore(score);
+
+            match.Player1.Score.Should().Be(score);
+        }
+
+        [Fact]
+        public void CanDecrementPlayerScore()
+        {
+            SystemTimeMocker.Set(match.StartDateTime.AddMinutes(1));
+
+            match.Player1.IncreaseScore(2);
+            match.Player1.DecreaseScore(1);
+
+            match.Player1.Score.Should().Be(1);
+        }
+
+        [Fact]
+        public void CannotDecreasePlayerScoreBelowZero()
+        {
+            SystemTimeMocker.Set(match.StartDateTime.AddMinutes(1));
+
+            match.Player1.DecreaseScore(1);
+
+            match.Player1.Score.Should().Be(0);
+        }
+    }
+}
