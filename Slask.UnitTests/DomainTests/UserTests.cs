28dc13a2f7449ff4bb894de2b69ee5cb55600637
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2019-12-19 21:12:26 +0100

    Clean User unit tests file

--- Slask.UnitTests/DomainTests/UserTests.cs
+++ Slask.UnitTests/DomainTests/UserTests.cs
@@ -7,7 +7,7 @@ namespace Slask.UnitTests.DomainTests
     public class UserTests
     {
         [Fact]
-        public void EnsureUserIsValidWhenCreated()
+        public void CanCreateUser()
         {
             User user = User.Create("Stålberto");
 
@@ -23,29 +23,5 @@ namespace Slask.UnitTests.DomainTests
 
             user.Should().BeNull();
         }
-
-        //[Fact]
-        //public void CannotCreateUserWithNameAlreadyInUseNoMatterLetterCasing()
-        //{
-        //    UserServiceContext services = GivenServices();
-        //    User createdUser = services.WhenCreatedUser();
-        //    User duplicateUser = services.UserService.CreateUser(createdUser.Name.ToUpper());
-
-        //    duplicateUser.Should().BeNull();
-        //}
-
-        //[Fact]
-        //public void CannotCreateUserWithEmptyName()
-        //{
-        //    UserServiceContext services = GivenServices();
-        //    User user = services.UserService.CreateUser("");
-
-        //    user.Should().BeNull();
-        //}
-
-        //private UserServiceContext GivenServices()
-        //{
-        //    return UserServiceContext.GivenServices(new UnitTestSlaskContextCreator());
-        //}
     }
 }
