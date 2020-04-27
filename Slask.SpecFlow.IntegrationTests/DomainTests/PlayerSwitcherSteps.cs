d8f038ee126c255ce9c5319f9288fb3e5542eaf3
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-04-27 20:20:04 +0200

    Create Specflow method that switches players in two different matches

--- Slask.SpecFlow.IntegrationTests/DomainTests/PlayerSwitcherSteps.cs
+++ Slask.SpecFlow.IntegrationTests/DomainTests/PlayerSwitcherSteps.cs
@@ -1,4 +1,7 @@
-﻿using Slask.SpecFlow.IntegrationTests.DomainTests.GroupTests;
+﻿using Slask.Domain;
+using Slask.Domain.Groups.Bases;
+using Slask.Domain.Groups.GroupUtility;
+using Slask.SpecFlow.IntegrationTests.DomainTests.GroupTests;
 using System;
 using TechTalk.SpecFlow;
 
@@ -12,6 +15,36 @@ namespace Slask.SpecFlow.IntegrationTests.DomainTests
 
     public class PlayerSwitcherStepDefinitions : GroupStepDefinitions
     {
+        [Given(@"player ""(.*)"" in group (.*) and player ""(.*)"" in group (.*) switches matches")]
+        [When(@"player ""(.*)"" in group (.*) and player ""(.*)"" in group (.*) switches matches")]
+        public void PlayerInGroupAndPlayerInGroupSwitchesMatches(string player1Name, int group1Index, string player2Name, int group2Index)
+        {
+            GroupBase group1 = createdGroups[group1Index];
+            GroupBase group2 = createdGroups[group2Index];
+
+            Player player1 = FindPlayerInGroup(player1Name, group1);
+            Player player2 = FindPlayerInGroup(player2Name, group2);
+
+            PlayerSwitcher.SwitchMatchesOn(player1, player2);
+        }
+
+        private Player FindPlayerInGroup(string playerName, GroupBase group)
+        {
+            foreach (Match match in group.Matches)
+            {
+                if (match.Player1.Name == playerName)
+                {
+                    return match.Player1;
+                }
+
+                if (match.Player2.Name == playerName)
+                {
+                    return match.Player2;
+                }
+            }
+
+            return null;
+        }
 
     }
 }
