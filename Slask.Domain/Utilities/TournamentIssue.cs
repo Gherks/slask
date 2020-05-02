394935080b129b30e5adeb982d399415c33c6935
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-05-02 18:18:42 +0200

    Implement TournamentIssueReporter

---
+++ Slask.Domain/Utilities/TournamentIssue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slask.Domain.Utilities
+{
+    public class TournamentIssue
+    {
+        private TournamentIssue()
+        {
+
+        }
+
+        public int Round { get; private set; }
+        public int Group { get; private set; }
+        public int Match { get; private set; }
+        public string Description { get; private set; }
+
+        public static TournamentIssue Create(int round, int group, int match, string description)
+        {
+            return new TournamentIssue()
+            {
+                Round = round,
+                Group = group,
+                Match = match,
+                Description = description
+            };
+        }
+    }
+}
