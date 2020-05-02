02518d490c0b6144941ec5ac011852a8ed72083c
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-05-02 13:27:21 +0200

    Create first set of tests for issue reporter

---
+++ Slask.UnitTests/DomainTests/TournamentIssueReporterTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Slask.UnitTests.DomainTests
+{
+    public class TournamentIssueReporterTests
+    {
+        [Fact]
+        public void CanReportOnAnIssue()
+        {
+
+        }
+
+        [Fact]
+        public void CanClearReports()
+        {
+
+        }
+
+        [Fact]
+        public void CanFetchAllIssues()
+        {
+
+        }
+
+        [Fact]
+        public void CanReportOnRoundIssueCorrectly()
+        {
+
+        }
+
+        [Fact]
+        public void CanReportOnGroupIssueCorrectly()
+        {
+
+        }
+
+        [Fact]
+        public void CanReportOnMatchIssueCorrectly()
+        {
+
+        }
+    }
+}
