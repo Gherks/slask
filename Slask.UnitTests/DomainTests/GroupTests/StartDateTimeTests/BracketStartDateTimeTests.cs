8b23f475df31c7bcb4831e127a1b87257b600899
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-05-04 13:33:30 +0200

    Rework som unit tests
    Move start date time tests for different kinds of groups to their own test files

---
+++ Slask.UnitTests/DomainTests/GroupTests/StartDateTimeTests/BracketStartDateTimeTests.cs
@@ -0,0 +1,93 @@
+﻿using FluentAssertions;
+using Slask.Common;
+using Slask.Domain;
+using Slask.Domain.Groups;
+using Slask.Domain.Groups.GroupUtility;
+using Slask.Domain.Rounds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Slask.UnitTests.DomainTests.GroupTests.StartDateTimeTests
+{
+    public class BracketStartDateTimeTests : IDisposable
+    {
+        private const string firstPlayerName = "Maru";
+        private const string secondPlayerName = "Stork";
+
+        private readonly Tournament tournament;
+        private readonly BracketRound bracketRound;
+        private BracketGroup bracketGroup;
+        //private Match match;
+        //private PlayerReference firstPlayerReference;
+        //private PlayerReference secondPlayerReference;
+
+        public BracketStartDateTimeTests()
+        {
+            tournament = Tournament.Create("GSL 2019");
+            bracketRound = tournament.AddBracketRound("Bracket round", 3) as BracketRound;
+        }
+
+        public void Dispose()
+        {
+            SystemTimeMocker.Reset();
+        }
+
+        [Fact]
+        public void StartDateTimeOnMatchesWithinATierDoesNotHaveToBeInOrder()
+        {
+            List<string> playerNames = new List<string>() { "Maru", "Stork", "Taeja", "Rain", "Bomber", "FanTaSy", "Stephano", "Thorzain" };
+            bracketRound.SetPlayersPerGroupCount(playerNames.Count);
+
+            foreach (string playerName in playerNames)
+            {
+                bracketRound.RegisterPlayerReference(playerName);
+            }
+
+            bracketGroup = bracketRound.Groups.First() as BracketGroup;
+            List<BracketNode> quarterfinalTier = bracketGroup.BracketNodeSystem.GetBracketNodesInTier(2);
+
+            DateTime twoHoursEarlier = quarterfinalTier[0].Match.StartDateTime.AddHours(-2);
+            DateTime threeHoursEarlier = quarterfinalTier[1].Match.StartDateTime.AddHours(-3);
+            DateTime oneHourEarlier = quarterfinalTier[2].Match.StartDateTime.AddHours(-1);
+            DateTime twoHoursLater = quarterfinalTier[3].Match.StartDateTime.AddHours(2);
+
+            quarterfinalTier[0].Match.SetStartDateTime(twoHoursEarlier);
+            quarterfinalTier[1].Match.SetStartDateTime(threeHoursEarlier);
+            quarterfinalTier[2].Match.SetStartDateTime(oneHourEarlier);
+            quarterfinalTier[3].Match.SetStartDateTime(twoHoursLater);
+
+            quarterfinalTier[0].Match.StartDateTime.Should().Be(twoHoursEarlier);
+            quarterfinalTier[1].Match.StartDateTime.Should().Be(threeHoursEarlier);
+            quarterfinalTier[2].Match.StartDateTime.Should().Be(oneHourEarlier);
+            quarterfinalTier[3].Match.StartDateTime.Should().Be(twoHoursLater);
+        }
+
+        [Fact]
+        public void StartDateTimeForMatchesInACertainMatchTierMustAlwaysBeLaterThanLatestStartDateTimeOfPreviousTier()
+        {
+            List<string> playerNames = new List<string>() { "Maru", "Stork", "Taeja", "Rain", "Bomber", "FanTaSy", "Stephano", "Thorzain" };
+            bracketRound.SetPlayersPerGroupCount(playerNames.Count);
+
+            foreach (string playerName in playerNames)
+            {
+                bracketRound.RegisterPlayerReference(playerName);
+            }
+
+            bracketGroup = bracketRound.Groups.First() as BracketGroup;
+            List<BracketNode> finalTier = bracketGroup.BracketNodeSystem.GetBracketNodesInTier(0);
+            List<BracketNode> semifinalTier = bracketGroup.BracketNodeSystem.GetBracketNodesInTier(1);
+            List<BracketNode> quarterfinalTier = bracketGroup.BracketNodeSystem.GetBracketNodesInTier(2);
+
+            DateTime finalStartDateTimeBeforeChange = finalTier[0].Match.StartDateTime;
+            DateTime quarterfinalStartDateTimeBeforeChange = quarterfinalTier[0].Match.StartDateTime;
+
+            finalTier[0].Match.SetStartDateTime(semifinalTier[0].Match.StartDateTime.AddHours(-4));
+            quarterfinalTier[0].Match.SetStartDateTime(semifinalTier[0].Match.StartDateTime.AddHours(4));
+
+            finalTier[0].Match.StartDateTime.Should().Be(finalStartDateTimeBeforeChange);
+            quarterfinalTier[0].Match.StartDateTime.Should().Be(quarterfinalStartDateTimeBeforeChange);
+        }
+    }
+}
---
+++ Slask.UnitTests/DomainTests/GroupTests/StartDateTimeTests/DualTournamentStartDateTimeTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using Slask.Common;
+using Slask.Domain;
+using Slask.Domain.Groups;
+using Slask.Domain.Rounds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Slask.UnitTests.DomainTests.GroupTests.StartDateTimeTests
+{
+    public class DualTournamentStartDateTimeTests : IDisposable
+    {
+        private readonly Tournament tournament;
+        private readonly DualTournamentRound dualTournamentRound;
+
+        public DualTournamentStartDateTimeTests()
+        {
+            tournament = Tournament.Create("GSL 2019");
+            dualTournamentRound = tournament.AddDualTournamentRound("Dual tournament round", 3) as DualTournamentRound;
+        }
+
+        public void Dispose()
+        {
+            SystemTimeMocker.Reset();
+        }
+
+        [Fact]
+        public void StartDateTimeForMatchesMustBeInMatchOrder()
+        {
+            DualTournamentGroup dualTournamentGroup = RegisterPlayers(new List<string> { "Maru", "Stork", "Taeja", "Rain" });
+
+            List<DateTime> dateTimesBeforeChange = new List<DateTime>();
+
+            foreach (Match match in dualTournamentGroup.Matches)
+            {
+                dateTimesBeforeChange.Add(match.StartDateTime);
+            }
+
+            DateTime twoHoursLater = dualTournamentGroup.Matches[0].StartDateTime.AddHours(2);
+            DateTime threeHoursEarlier = dualTournamentGroup.Matches[1].StartDateTime.AddHours(-3);
+            DateTime fourHoursLater = dualTournamentGroup.Matches[2].StartDateTime.AddHours(4);
+            DateTime threeHoursLater = dualTournamentGroup.Matches[3].StartDateTime.AddHours(3);
+            DateTime nineHoursEarlier = dualTournamentGroup.Matches[4].StartDateTime.AddHours(-9);
+
+            dualTournamentGroup.Matches[0].SetStartDateTime(twoHoursLater);
+            dualTournamentGroup.Matches[1].SetStartDateTime(threeHoursEarlier);
+            dualTournamentGroup.Matches[2].SetStartDateTime(fourHoursLater);
+            dualTournamentGroup.Matches[3].SetStartDateTime(threeHoursLater);
+            dualTournamentGroup.Matches[4].SetStartDateTime(nineHoursEarlier);
+
+            for (int matchIndex = 0; matchIndex < dualTournamentGroup.Matches.Count; ++matchIndex)
+            {
+                dualTournamentGroup.Matches[matchIndex].StartDateTime.Should().Be(dateTimesBeforeChange[matchIndex]);
+            }
+        }
+
+        private DualTournamentGroup RegisterPlayers(List<string> playerNames)
+        {
+            foreach (string playerName in playerNames)
+            {
+                dualTournamentRound.RegisterPlayerReference(playerName);
+            }
+
+            return dualTournamentRound.Groups.First() as DualTournamentGroup;
+        }
+    }
+}
