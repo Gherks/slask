bfcc5aad128595f5a6e9dc4ad6e6cfbb4dcf8322
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-05-21 01:58:32 +0200

    Add procedures for advancing per group count

---
+++ Slask.Domain/Procedures/AdvancingPerGroupCount/AdvancingPerGroupCountProcedure.cs
@@ -0,0 +1,10 @@
+﻿using Slask.Domain.Rounds.Bases;
+
+namespace Slask.Domain.Procedures.AdvancingPerGroupCount
+{
+    public abstract class AdvancingPerGroupCountProcedure : ProcedureInterface<int, RoundBase>
+    {
+        public abstract bool NewValueValid(int inValue, out int outValue, RoundBase parent);
+        public abstract void ApplyPostAssignmentOperations(RoundBase parent);
+    }
+}
---
+++ Slask.Domain/Procedures/AdvancingPerGroupCount/ImmutableAdvancingPerGroupCountProcedure.cs
@@ -0,0 +1,17 @@
+﻿using Slask.Domain.Rounds.Bases;
+
+namespace Slask.Domain.Procedures.AdvancingPerGroupCount
+{
+    public class ImmutableAdvancingPerGroupCountProcedure : AdvancingPerGroupCountProcedure
+    {
+        public override bool NewValueValid(int inValue, out int outValue, RoundBase parent)
+        {
+            outValue = -1;
+            return false;
+        }
+
+        public override void ApplyPostAssignmentOperations(RoundBase parent)
+        {
+        }
+    }
+}
