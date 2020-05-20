161d675ad46e734ad703729599c4daa37fb238b7
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-05-21 00:54:47 +0200

    Add procedure interface

---
+++ Slask.Domain/Procedure/ProcedureInterface.cs
@@ -0,0 +1,7 @@
+﻿namespace Slask.Domain.Procedure
+{
+    public interface ProcedureInterface<Type, ParentType>
+    {
+        bool Set(Type inValue, out Type outValue, ParentType parent);
+    }
+}
