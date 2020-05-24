c46e71df2886f937de4892d3bfb5b2602470aa7a
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-05-25 00:39:17 +0200

    Create enum of every type of tournament issue

---
+++ Slask.Domain/Utilities/TournamentIssueReporter/TournamentIssues.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slask.Domain.Utilities
+{
+    public enum TournamentIssues
+    {
+        NotFillingAllGroupsWithPlayers,
+        RoundDoesNotSynergizeWithPreviousRound,
+        AdvancersCountInRoundIsGreaterThanParticipantCount,
+        LastRoundContainsMoreThanOneGroup,
+        LastRoundHasMoreThanOneAdvancers,
+        StartDateTimeIsInThePast,
+        StartDateTimeIncompatibleWithPreviousRound,
+        StartDateTimeIncompatibleWithGroupRules
+    }
+}
