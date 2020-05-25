d48cd30f7b928695526a41a9e7a402d0673dca31
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-05-25 22:34:51 +0200

    Create bet interface

---
+++ Slask.Domain/Bets/BetInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Slask.Domain.Bets
+{
+    public interface BetInterface
+    {
+        Better Better { get; }
+        Guid BetterId { get; }
+        Guid Id { get; }
+    }
+}
