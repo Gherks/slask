0b8e0b5f27bc2a119b6ec02e4c8458caa944ee1b
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-05-29 23:55:53 +0200

    Complete Specflow tests for betters

--- Test/Slask.SpecFlow.IntegrationTests/DomainTests/Better.feature
+++ Test/Slask.SpecFlow.IntegrationTests/DomainTests/Better.feature
@@ -1,4 +1,67 @@
 ﻿Feature: Better
 	Does a bunch of tests on Betters
 
-#@BetterTag
+@BetterTag
+Scenario: Can deteremine correct better standings
+	Given a tournament named "GSL 2019" has been created with users "Stålberto, Bönis, Guggelito, Kimmieboi" added to it
+		And tournament 0 adds rounds
+			| Round type | Advancing per group count | Players per group count |
+			| Bracket    | 1                         | 4                       |
+			| Bracket    | 1                         | 2                       |
+		And players "Maru, Stork, Taeja, Rain, Bomber, FanTaSy, Stephano, Thorzain" is registered to round 0
+		And betters places match bets
+			| Better name | Round index | Group index | Match index | Player name |
+			| Stålberto   | 0           | 0           | 0           | Maru        |
+			| Stålberto   | 0           | 0           | 1           | Taeja       |
+			| Stålberto   | 0           | 1           | 0           | Bomber      |
+			| Stålberto   | 0           | 1           | 1           | Stephano    |
+			| Bönis       | 0           | 0           | 0           | Maru        |
+			| Bönis       | 0           | 0           | 1           | Taeja       |
+			| Bönis       | 0           | 1           | 0           | FanTaSy     |
+			| Bönis       | 0           | 1           | 1           | Thorzain    |
+			| Guggelito   | 0           | 0           | 0           | Stork       |
+			| Guggelito   | 0           | 0           | 1           | Rain        |
+			| Guggelito   | 0           | 1           | 0           | FanTaSy     |
+			| Guggelito   | 0           | 1           | 1           | Stephano    |
+			| Kimmieboi   | 0           | 0           | 0           | Stork       |
+			| Kimmieboi   | 0           | 0           | 1           | Rain        |
+			| Kimmieboi   | 0           | 1           | 0           | Bomber      |
+			| Kimmieboi   | 0           | 1           | 1           | Thorzain    |
+	When groups within tournament is played out and betted on
+			| Tournament index | Round index | Group index |
+			| 0                | 0           | 0           |
+			| 0                | 0           | 1           |
+	Then player standings in tournament 0 from first to last looks like this
+		| Better name | Points |
+		| Stålberto   | 3      |
+		| Guggelito   | 2      |
+		| Kimmieboi   | 2      |
+		| Bönis       | 1      |
+
+Scenario: Yields empty list when fetching better standings in tournament that has not started
+	Given a tournament named "GSL 2019" has been created with users "Stålberto, Bönis, Guggelito, Kimmieboi" added to it
+		And tournament 0 adds rounds
+			| Round type | Advancing per group count | Players per group count |
+			| Bracket    | 1                         | 4                       |
+			| Bracket    | 1                         | 2                       |
+		And players "Maru, Stork, Taeja, Rain, Bomber, FanTaSy, Stephano, Thorzain" is registered to round 0
+	When betters places match bets
+		| Better name | Round index | Group index | Match index | Player name |
+		| Stålberto   | 0           | 0           | 0           | Maru        |
+		| Stålberto   | 0           | 0           | 1           | Taeja       |
+		| Stålberto   | 0           | 1           | 0           | Bomber      |
+		| Stålberto   | 0           | 1           | 1           | Stephano    |
+		| Bönis       | 0           | 0           | 0           | Maru        |
+		| Bönis       | 0           | 0           | 1           | Taeja       |
+		| Bönis       | 0           | 1           | 0           | FanTaSy     |
+		| Bönis       | 0           | 1           | 1           | Thorzain    |
+		| Guggelito   | 0           | 0           | 0           | Stork       |
+		| Guggelito   | 0           | 0           | 1           | Rain        |
+		| Guggelito   | 0           | 1           | 0           | FanTaSy     |
+		| Guggelito   | 0           | 1           | 1           | Stephano    |
+		| Kimmieboi   | 0           | 0           | 0           | Stork       |
+		| Kimmieboi   | 0           | 0           | 1           | Rain        |
+		| Kimmieboi   | 0           | 1           | 0           | Bomber      |
+		| Kimmieboi   | 0           | 1           | 1           | Thorzain    |
+	Then solving player standings in tournament 0 is ordered from first to last "" 
+		
---
+++ Test/Slask.SpecFlow.IntegrationTests/DomainTests/BetterSteps.cs
@@ -0,0 +1,142 @@
+﻿using FluentAssertions;
+using Slask.Common;
+using Slask.Domain;
+using Slask.Domain.Groups;
+using Slask.Domain.Rounds;
+using Slask.SpecFlow.IntegrationTests.DomainTests.RoundTests;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace Slask.SpecFlow.IntegrationTests.DomainTests
+{
+    [Binding, Scope(Feature = "Better")]
+    public class BetterSteps : BetterStepDefinitions
+    {
+
+    }
+
+    public class BetterStepDefinitions : RoundStepDefinitions
+    {
+        [Given(@"betters places match bets")]
+        [When(@"betters places match bets")]
+        public void WhenBettersPlacesMatchBets(Table table)
+        {
+            foreach (TableRow row in table.Rows)
+            {
+                ParseBetterMatchBetPlacements(row, out string betterName, out int roundIndex, out int groupIndex, out int matchIndex, out string playerName);
+
+                RoundBase round = createdRounds[roundIndex];
+                GroupBase group = createdGroups[groupIndex];
+                Match match = group.Matches[matchIndex];
+
+                bool betterNameIsNotEmpty = betterName.Length > 0;
+                bool playerNameIsNotEmpty = playerName.Length > 0;
+
+                if (betterNameIsNotEmpty && playerNameIsNotEmpty)
+                {
+                    Better better = round.Tournament.GetBetterByName(betterName);
+                    Player player = match.FindPlayer(playerName);
+
+                    better.Should().NotBeNull();
+                    player.Should().NotBeNull();
+
+                    better.PlaceMatchBet(match, player).Should().BeTrue();
+                }
+            }
+        }
+
+        [Then(@"player standings in tournament (.*) from first to last looks like this")]
+        public void ThenPlayerStandingsFromFirstToLastLooksLikeThis(int tournamentIndex, Table table)
+        {
+            Tournament tournament = createdTournaments[tournamentIndex];
+
+            List<BetterEntry> betterStandings = BetterStandingsSolver.Solve(tournament);
+
+            betterStandings.Should().HaveCount(table.Rows.Count);
+
+            for(int index = 0; index < table.Rows.Count; ++index)
+            {
+                ParseBetterStandings(table.Rows[index], out string betterName, out int points);
+
+                betterStandings[index].Name.Should().Be(betterName);
+                betterStandings[index].Points.Should().Be(points);
+            }
+        }
+
+        [Then(@"solving player standings in tournament (.*) is ordered from first to last ""(.*)""")]
+        public void ThenSolvingPlayerStandingsIsOrderedFromFirstToLast(int tournamentIndex, string commaSeparatedBetterNames)
+        {
+            List<string> betterNames = StringUtility.ToStringList(commaSeparatedBetterNames, ",");
+
+            Tournament tournament = createdTournaments[tournamentIndex];
+
+            List<BetterEntry> betterStandings = BetterStandingsSolver.Solve(tournament);
+
+            betterStandings.Should().HaveCount(betterNames.Count);
+
+            for(int index = 0; index < betterStandings.Count; ++index)
+            {
+                betteStandings[index].Name.Should().Be(betterNames[index]);
+            }
+        }
+
+        private void ParseBetterMatchBetPlacements(TableRow row, out string betterName, out int roundIndex, out int groupIndex, out int matchIndex, out string playerName)
+        {
+            betterName = "";
+            roundIndex = -1;
+            groupIndex = -1;
+            matchIndex = -1;
+            playerName = "";
+
+            if (row.ContainsKey("Better name"))
+            {
+                betterName = row["Better name"];
+            }
+
+            if (row.ContainsKey("Round index"))
+            {
+                int.TryParse(row["Round index"], out roundIndex);
+            }
+
+            roundIndex.Should().BeGreaterOrEqualTo(0);
+
+            if (row.ContainsKey("Group index"))
+            {
+                int.TryParse(row["Group index"], out groupIndex);
+            }
+
+            groupIndex.Should().BeGreaterOrEqualTo(0);
+
+            if (row.ContainsKey("Match index"))
+            {
+                int.TryParse(row["Match index"], out matchIndex);
+            }
+
+            matchIndex.Should().BeGreaterOrEqualTo(0);
+
+            if (row.ContainsKey("Player name"))
+            {
+                playerName = row["Player name"];
+            }
+        }
+
+        private void ParseBetterStandings(TableRow row, out string betterName, out int points)
+        {
+            betterName = "";
+            points = -1;
+
+            if (row.ContainsKey("Better name"))
+            {
+                betterName = row["Better name"];
+            }
+
+            if (row.ContainsKey("Points"))
+            {
+                int.TryParse(row["Points"], out points);
+            }
+
+            points.Should().BeGreaterOrEqualTo(0);
+        }
+    }
+}
