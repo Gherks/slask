20db442172df31ca74d83007424c8a4e947ecf3b
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-08-17 22:31:24 +0200

    Add new interface SortableInterface
    Sets up a contract of how an entity should handle sorting

---
+++ Slask.Domain/SortableInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slask.Domain
+{
+    public interface SortableInterface
+    {
+        public int SortOrder { get; }
+
+        public void UpdateSortOrder();
+    }
+}
