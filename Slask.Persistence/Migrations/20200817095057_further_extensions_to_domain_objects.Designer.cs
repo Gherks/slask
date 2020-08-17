87480af9bcc0083999ff607480d27e3172069576
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-08-17 12:04:17 +0200

    Add newest migration

--- Slask.Persistence/Migrations/20200817095057_further_extensions_to_domain_objects.Designer.cs
+++ Slask.Persistence/Migrations/20200817095057_further_extensions_to_domain_objects.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Slask.Persistence;
 
 namespace Slask.Persistence.Migrations
 {
     [DbContext(typeof(SlaskContext))]
-    partial class SlaskContextModelSnapshot : ModelSnapshot
+    [Migration("20200817095057_further_extensions_to_domain_objects")]
+    partial class further_extensions_to_domain_objects
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
