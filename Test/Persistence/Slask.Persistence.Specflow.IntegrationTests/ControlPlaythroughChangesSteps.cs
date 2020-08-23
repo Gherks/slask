7c269fd44c08f5af665d499daf237d50320fef2e
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-08-23 23:10:43 +0200

    Add new Specflow test for Persistence layer

---
+++ Test/Persistence/Slask.Persistence.Specflow.IntegrationTests/ControlPlaythroughChangesSteps.cs
@@ -0,0 +1,11 @@
+﻿using Slask.SpecFlow.IntegrationTests.PersistenceTests;
+using TechTalk.SpecFlow;
+
+namespace Slask.Persistence.Specflow.IntegrationTests
+{
+    [Binding, Scope(Feature = "ControlPlaythroughChanges")]
+    public class ControlPlaythroughChangesSteps : PersistenceSteps
+    {
+        
+    }
+}
