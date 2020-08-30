ac45287533ced88838b841128aaabbec47ce2a2e
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-08-30 21:55:25 +0200

    Add some basic things to all commands

--- Slask.Application/Commands/DeleteRoundFromTournamentByGuidCommand.cs
+++ Slask.Application/Commands/DeleteRoundFromTournamentByGuidCommand.cs
@@ -1,7 +1,21 @@
-﻿namespace Slask.Application.Commands
+﻿using Slask.Application.Commands.Interfaces;
+using Slask.Persistence.Services;
+
+namespace Slask.Application.Commands
 {
     class DeleteRoundFromTournamentByGuidCommand : DeleteRoundFromTournamentByGuidCommandInterface
     {
+        private readonly UserServiceInterface _userServiceInterface;
+        private readonly TournamentServiceInterface _tournamentServiceInterface;
+
+        public DeleteRoundFromTournamentByGuidCommand(
+                UserServiceInterface userServiceInterface,
+                TournamentServiceInterface tournamentServiceInterface)
+        {
+            _userServiceInterface = userServiceInterface;
+            _tournamentServiceInterface = tournamentServiceInterface;
+        }
+
         public void Execute()
         {
 
--- Slask.Application/Commands/DeleteTournamentByNameCommand.cs
+++ Slask.Application/Commands/DeleteTournamentByNameCommand.cs
@@ -1,7 +1,21 @@
-﻿namespace Slask.Application.Commands
+﻿using Slask.Application.Commands.Interfaces;
+using Slask.Persistence.Services;
+
+namespace Slask.Application.Commands
 {
     class DeleteTournamentByNameCommand : DeleteTournamentByNameCommandInterface
     {
+        private readonly UserServiceInterface _userServiceInterface;
+        private readonly TournamentServiceInterface _tournamentServiceInterface;
+
+        public DeleteTournamentByNameCommand(
+                UserServiceInterface userServiceInterface,
+                TournamentServiceInterface tournamentServiceInterface)
+        {
+            _userServiceInterface = userServiceInterface;
+            _tournamentServiceInterface = tournamentServiceInterface;
+        }
+
         public void Execute()
         {
 
