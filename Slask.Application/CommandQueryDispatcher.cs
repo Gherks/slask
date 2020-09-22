54c82617bc59db9aff4c957c4bc5035123654ffd
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 08:10:14 +0200

    Add dispatcher for commands and queries

---
+++ Slask.Application/CommandQueryDispatcher.cs
@@ -0,0 +1,43 @@
+﻿using CSharpFunctionalExtensions;
+using Slask.Application.Commands.Interfaces;
+using Slask.Application.Queries.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slask.Application
+{
+    public sealed class CommandQueryDispatcher
+    {
+        private readonly IServiceProvider _provider;
+
+        public CommandQueryDispatcher(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public Result Dispatch(CommandInterface command)
+        {
+            Type type = typeof(CommandHandlerInterface<>);
+            Type[] typeArgs = { command.GetType() };
+            Type commandHandlerType = type.MakeGenericType(typeArgs);
+
+            dynamic handler = _provider.GetService(commandHandlerType);
+            Result result =  handler.Handle((dynamic)command);
+
+            return result;
+        }
+
+        public ResultType Dispatch<ResultType>(QueryInterface<ResultType> query)
+        {
+            Type type = typeof(QueryHandlerInterface<,>);
+            Type[] typeArgs = { query.GetType(), typeof(ResultType) };
+            Type queryHandlerType = type.MakeGenericType(typeArgs);
+
+            dynamic handler = _provider.GetService(queryHandlerType);
+            ResultType result = handler.Handle((dynamic)query);
+
+            return result;
+        }
+    }
+}
