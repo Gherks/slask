8dc4c6f4d82d9aa8e7f84304a81ded22b4badcd4
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 19:43:19 +0200

    Rename command

---
+++ Slask.Application/Commands/AddScoreToPlayerInMatch.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+using Slask.Application.Commands.Interfaces;
+using Slask.Persistence.Services;
+
+namespace Slask.Application.Commands
+{
+    public sealed class AddScoreToPlayerInMatch : CommandInterface
+    {
+    }
+
+    public sealed class AddScoreToPlayerInMatchHandler : CommandHandlerInterface<AddScoreToPlayerInMatch>
+    {
+        private readonly TournamentServiceInterface _tournamentService;
+
+        public AddScoreToPlayerInMatchHandler(TournamentServiceInterface tournamentService)
+        {
+            _tournamentService = tournamentService;
+        }
+
+        public Result Handle(AddScoreToPlayerInMatch command)
+        {
+            return Result.Success();
+        }
+    }
+}
