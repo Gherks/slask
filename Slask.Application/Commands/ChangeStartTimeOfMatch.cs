50945fc134a9230e6210fe99a3c8ab3ca0867e36
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 21:23:37 +0200

    Implement ChangeStartTimeOfMatch command

--- Slask.Application/Commands/ChangeStartTimeOfMatch.cs
+++ Slask.Application/Commands/ChangeStartTimeOfMatch.cs
@@ -1,28 +1,58 @@
 ﻿using CSharpFunctionalExtensions;
 using Slask.Application.Commands.Interfaces;
+using Slask.Domain;
 using Slask.Persistence.Services;
+using System;
 
 namespace Slask.Application.Commands
 {
     public sealed class ChangeStartTimeOfMatch : CommandInterface
     {
+        public Guid TournamentId { get; }
+        public Guid MatchId { get; }
+        public DateTime StartDateTime { get; }
+
+        public ChangeStartTimeOfMatch(Guid tournamentId, Guid matchId, DateTime startDateTime)
+        {
+            TournamentId = tournamentId;
+            MatchId = matchId;
+            StartDateTime = startDateTime;
+        }
     }
 
     public sealed class ChangeStartTimeOfMatchHandler : CommandHandlerInterface<ChangeStartTimeOfMatch>
     {
-        private readonly UserServiceInterface _userService;
         private readonly TournamentServiceInterface _tournamentService;
 
-        public ChangeStartTimeOfMatchHandler(
-                UserServiceInterface userService,
-                TournamentServiceInterface tournamentService)
+        public ChangeStartTimeOfMatchHandler(TournamentServiceInterface tournamentService)
         {
-            _userService = userService;
             _tournamentService = tournamentService;
         }
 
         public Result Handle(ChangeStartTimeOfMatch command)
         {
+            Tournament tournament = _tournamentService.GetTournamentById(command.TournamentId);
+
+            if (tournament == null)
+            {
+                return Result.Failure($"Could not change start time ({ command.StartDateTime }) in match ({ command.MatchId }). Tournament ({ command.TournamentId }) not found.");
+            }
+
+            Match match = tournament.GetMatchByMatchId(command.MatchId);
+
+            if (match == null)
+            {
+                return Result.Failure($"Could not change start time ({ command.StartDateTime }) in match ({ command.MatchId }). Match not found.");
+            }
+
+            bool changeSuccessfull = _tournamentService.SetStartTimeForMatch(match, command.StartDateTime);
+
+            if (!changeSuccessfull)
+            {
+                return Result.Failure($"Could not change start time ({ command.StartDateTime }) in match ({ command.MatchId }).");
+            }
+
+            _tournamentService.Save();
             return Result.Success();
         }
     }
