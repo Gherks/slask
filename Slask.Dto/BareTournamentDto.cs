cdc5168a68cf3b426fa85a4033e6101071d4cedb
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 11:38:20 +0200

    Create BareTournamentDto
    This dto contains only the id, name, and created date of a tournament

---
+++ Slask.Dto/BareTournamentDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Slask.Dto
+{
+    public sealed class BareTournamentDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
