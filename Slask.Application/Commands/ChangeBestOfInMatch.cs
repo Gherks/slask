70c265626c7db4615533d00bf8581384d291b101
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 21:13:34 +0200

    Implement ChangeBestOfInMatch command

--- Slask.Application/Commands/ChangeBestOfInMatch.cs
+++ Slask.Application/Commands/ChangeBestOfInMatch.cs
@@ -1,28 +1,58 @@
 ﻿using CSharpFunctionalExtensions;
 using Slask.Application.Commands.Interfaces;
+using Slask.Domain;
 using Slask.Persistence.Services;
+using System;
 
 namespace Slask.Application.Commands
 {
     public sealed class ChangeBestOfInMatch : CommandInterface
     {
+        public Guid TournamentId { get; }
+        public Guid MatchId { get; }
+        public int BestOf { get; }
+
+        public ChangeBestOfInMatch(Guid tournamentId, Guid matchId, int bestOf)
+        {
+            TournamentId = tournamentId;
+            MatchId = matchId;
+            BestOf = bestOf;
+        }
     }
 
     public sealed class ChangeBestOfInMatchHandler : CommandHandlerInterface<ChangeBestOfInMatch>
     {
-        private readonly UserServiceInterface _userService;
         private readonly TournamentServiceInterface _tournamentService;
 
-        public ChangeBestOfInMatchHandler(
-                UserServiceInterface userService,
-                TournamentServiceInterface tournamentService)
+        public ChangeBestOfInMatchHandler(TournamentServiceInterface tournamentService)
         {
-            _userService = userService;
             _tournamentService = tournamentService;
         }
 
         public Result Handle(ChangeBestOfInMatch command)
         {
+            Tournament tournament = _tournamentService.GetTournamentById(command.TournamentId);
+
+            if (tournament == null)
+            {
+                return Result.Failure($"Could not change best of ({ command.BestOf }) setting in match ({ command.MatchId }). Tournament ({ command.TournamentId }) not found.");
+            }
+
+            Match match = tournament.GetMatchByMatchId(command.MatchId);
+
+            if (match == null)
+            {
+                return Result.Failure($"Could not change best of ({ command.BestOf }) setting in match ({ command.MatchId }). Match not found.");
+            }
+
+            bool changeSuccessfull = _tournamentService.SetBestOfInMatch(match, command.BestOf);
+
+            if (!changeSuccessfull)
+            {
+                return Result.Failure($"Could not change best of ({ command.BestOf }) setting in match ({ command.MatchId }).");
+            }
+
+            _tournamentService.Save();
             return Result.Success();
         }
     }
