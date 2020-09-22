4cfb8235dbca9bd6c5b1b8987b8855707455f27e
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 08:09:15 +0200

    Add Dto project

---
+++ Slask.Dto/TournamentDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slask.Dto
+{
+    public sealed class TournamentDto
+    {
+    }
+}
---
+++ Slask.Dto/UserDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Slask.Dto
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
