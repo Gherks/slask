b55b8c0a007fc169ec0ed35c7f5002593880749b
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 19:55:28 +0200

    Implement AddScoreToPlayerInMatch command
    Create two different commands that either fetches tournament by id or name

---
+++ Slask.Application/Commands/AddScoreToPlayerInMatchByTournamentName.cs
@@ -0,0 +1,53 @@
+﻿using CSharpFunctionalExtensions;
+using Slask.Application.Commands.Interfaces;
+using Slask.Domain;
+using Slask.Persistence.Services;
+using System;
+
+namespace Slask.Application.Commands
+{
+    public sealed class AddScoreToPlayerInMatchByTournamentName : CommandInterface
+    {
+        public string TournamentName { get; }
+        public Guid MatchId { get; }
+        public Guid PlayerId { get; }
+        public int Score { get; }
+
+        public AddScoreToPlayerInMatchByTournamentName(string tournamentName, Guid matchId, Guid playerId, int score)
+        {
+            TournamentName = tournamentName;
+            MatchId = matchId;
+            PlayerId = playerId;
+            Score = score;
+        }
+    }
+
+    public sealed class AddScoreToPlayerInMatchByTournamentNameHandler : CommandHandlerInterface<AddScoreToPlayerInMatchByTournamentName>
+    {
+        private readonly TournamentServiceInterface _tournamentService;
+
+        public AddScoreToPlayerInMatchByTournamentNameHandler(TournamentServiceInterface tournamentService)
+        {
+            _tournamentService = tournamentService;
+        }
+
+        public Result Handle(AddScoreToPlayerInMatchByTournamentName command)
+        {
+            Tournament tournament = _tournamentService.GetTournamentByName(command.TournamentName);
+
+            if (tournament == null)
+            {
+                return Result.Failure($"Could add score ({ command.Score }) to player ({ command.PlayerId }) in match ({ command.MatchId }). Tournament ({ command.TournamentName }) not found.");
+            }
+
+            bool scoreAdded = _tournamentService.AddScoreToPlayerInMatch(tournament, command.MatchId, command.PlayerId, command.Score);
+
+            if (!scoreAdded)
+            {
+                return Result.Failure($"Could add score ({ command.Score }) to player ({ command.PlayerId }) in match ({ command.MatchId }).");
+            }
+
+            return Result.Success();
+        }
+    }
+}
