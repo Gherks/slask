f7bfe5d2e38520962083f770f469a2dc23faa008
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 21:27:13 +0200

    Implement RenameTournament command

--- Slask.Application/Commands/RenameTournament.cs
+++ Slask.Application/Commands/RenameTournament.cs
@@ -1,28 +1,41 @@
 ﻿using CSharpFunctionalExtensions;
 using Slask.Application.Commands.Interfaces;
 using Slask.Persistence.Services;
+using System;
 
 namespace Slask.Application.Commands
 {
     public sealed class RenameTournament : CommandInterface
     {
+        public Guid TournamentId { get; }
+        public string TournamentName { get; }
+
+        public RenameTournament(Guid tournamentId, string tournamentName)
+        {
+            TournamentId = tournamentId;
+            TournamentName = tournamentName;
+        }
     }
 
     public sealed class RenameTournamentHandler : CommandHandlerInterface<RenameTournament>
     {
-        private readonly UserServiceInterface _userService;
         private readonly TournamentServiceInterface _tournamentService;
 
-        public RenameTournamentHandler(
-                UserServiceInterface userService,
-                TournamentServiceInterface tournamentService)
+        public RenameTournamentHandler(TournamentServiceInterface tournamentService)
         {
-            _userService = userService;
             _tournamentService = tournamentService;
         }
 
         public Result Handle(RenameTournament command)
         {
+            bool renameSuccessfull = _tournamentService.RenameTournament(command.TournamentId, command.TournamentName);
+
+            if (!renameSuccessfull)
+            {
+                return Result.Failure($"Could not rename tournament ({ command.TournamentId }) to \"({ command.TournamentName })\"");
+            }
+
+            _tournamentService.Save();
             return Result.Success();
         }
     }
