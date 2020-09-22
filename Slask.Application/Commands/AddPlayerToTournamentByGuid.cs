7c50c6a2c4d0aed07695ec9896dbfe6915c809b5
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 19:36:46 +0200

    Create two commands for adding new player to tournament

---
+++ Slask.Application/Commands/AddPlayerToTournamentByGuid.cs
@@ -0,0 +1,49 @@
+﻿using CSharpFunctionalExtensions;
+using Slask.Application.Commands.Interfaces;
+using Slask.Domain;
+using Slask.Persistence.Services;
+using System;
+
+namespace Slask.Application.Commands
+{
+    public sealed class AddPlayerToTournamentByGuid : CommandInterface
+    {
+        public Guid TournamentId { get; }
+        public string PlayerName { get; }
+
+        public AddPlayerToTournamentByGuid(Guid tournamentId, string playerName)
+        {
+            TournamentId = tournamentId;
+            PlayerName = playerName;
+        }
+    }
+
+    public sealed class AddPlayerToTournamentByGuidHandler : CommandHandlerInterface<AddPlayerToTournamentByGuid>
+    {
+        private readonly TournamentServiceInterface _tournamentService;
+
+        public AddPlayerToTournamentByGuidHandler(TournamentServiceInterface tournamentService)
+        {
+            _tournamentService = tournamentService;
+        }
+
+        public Result Handle(AddPlayerToTournamentByGuid command)
+        {
+            Tournament tournament = _tournamentService.GetTournamentById(command.TournamentId);
+
+            if (tournament == null)
+            {
+                return Result.Failure($"Could not add new player ({ command.PlayerName }) to tournament. Tournament ({ command.TournamentId }) not found.");
+            }
+
+            PlayerReference playerReference = _tournamentService.AddPlayerReference(tournament, command.PlayerName);
+
+            if (playerReference == null)
+            {
+                return Result.Failure($"Could not add new player ({ command.PlayerName }) to tournament.");
+            }
+
+            return Result.Success();
+        }
+    }
+}
---
+++ Slask.Application/Commands/AddPlayerToTournamentByName.cs
@@ -0,0 +1,48 @@
+﻿using CSharpFunctionalExtensions;
+using Slask.Application.Commands.Interfaces;
+using Slask.Domain;
+using Slask.Persistence.Services;
+
+namespace Slask.Application.Commands
+{
+    public sealed class AddPlayerToTournamentByName : CommandInterface
+    {
+        public string TournamentName { get; }
+        public string PlayerName { get; }
+
+        public AddPlayerToTournamentByName(string tournamentName, string playerName)
+        {
+            TournamentName = tournamentName;
+            PlayerName = playerName;
+        }
+    }
+
+    public sealed class AddPlayerToTournamentByNameHandler : CommandHandlerInterface<AddPlayerToTournamentByName>
+    {
+        private readonly TournamentServiceInterface _tournamentService;
+
+        public AddPlayerToTournamentByNameHandler(TournamentServiceInterface tournamentService)
+        {
+            _tournamentService = tournamentService;
+        }
+
+        public Result Handle(AddPlayerToTournamentByName command)
+        {
+            Tournament tournament = _tournamentService.GetTournamentByName(command.TournamentName);
+
+            if (tournament == null)
+            {
+                return Result.Failure($"Could not add new player ({ command.PlayerName }) to tournament. Tournament ({ command.TournamentName }) not found.");
+            }
+
+            PlayerReference playerReference = _tournamentService.AddPlayerReference(tournament, command.PlayerName);
+
+            if (playerReference == null)
+            {
+                return Result.Failure($"Could not add new player ({ command.PlayerName }) to tournament.");
+            }
+
+            return Result.Success();
+        }
+    }
+}
