1ed66371bcbb518053be0976c8f279fa558730e7
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 20:52:48 +0200

    Implement BetterPlaceBetOnMatch command

--- Slask.Application/Commands/BetterPlaceBetOnMatch.cs
+++ Slask.Application/Commands/BetterPlaceBetOnMatch.cs
@@ -1,28 +1,48 @@
 ﻿using CSharpFunctionalExtensions;
 using Slask.Application.Commands.Interfaces;
 using Slask.Persistence.Services;
+using System;
 
 namespace Slask.Application.Commands
 {
     public sealed class BetterPlaceBetOnMatch : CommandInterface
     {
+        public Guid TournamentId { get; }
+        public Guid MatchId { get; }
+        public string BetterName { get; }
+        public string PlayerName { get; }
+
+        public BetterPlaceBetOnMatch(Guid tournamentId, Guid matchId, string betterName, string playerName)
+        {
+            TournamentId = tournamentId;
+            MatchId = matchId;
+            BetterName = betterName;
+            PlayerName = playerName;
+        }
     }
 
     public sealed class BetterPlaceBetOnMatchHandler : CommandHandlerInterface<BetterPlaceBetOnMatch>
     {
-        private readonly UserServiceInterface _userService;
         private readonly TournamentServiceInterface _tournamentService;
 
-        public BetterPlaceBetOnMatchHandler(
-                UserServiceInterface userService,
-                TournamentServiceInterface tournamentService)
+        public BetterPlaceBetOnMatchHandler(TournamentServiceInterface tournamentService)
         {
-            _userService = userService;
             _tournamentService = tournamentService;
         }
 
         public Result Handle(BetterPlaceBetOnMatch command)
         {
+            bool betPlaced = _tournamentService.BetterPlacesMatchBetOnMatch(
+                command.TournamentId,
+                command.MatchId,
+                command.BetterName,
+                command.PlayerName);
+
+            if (!betPlaced)
+            {
+                return Result.Failure($"Better ({ command.BetterName }) could not place match bet on player ({ command.PlayerName }) in match ({ command.MatchId }) within tournament ({ command.TournamentId })");
+            }
+
             return Result.Success();
         }
     }
