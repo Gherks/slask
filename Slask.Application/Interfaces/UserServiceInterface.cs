f80f2b697b1f9ecf0b7d471a18878e8ef15c770e
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-22 08:08:48 +0200

    Update user and tournament interfaces

--- Slask.Application/Interfaces/UserServiceInterface.cs
+++ Slask.Application/Interfaces/UserServiceInterface.cs
@@ -1,14 +1,16 @@
 ﻿using Slask.Domain;
 using System;
+using System.Collections.Generic;
 
 namespace Slask.Persistence.Services
 {
     public interface UserServiceInterface
     {
         User CreateUser(string name);
-        User GetUserById(Guid id);
-        User GetUserByName(string name);
         bool RenameUser(Guid id, string name);
+        IEnumerable<User> GetUsers();
+        User GetUserByName(string name);
+        User GetUserById(Guid id);
         void Save();
     }
 }
