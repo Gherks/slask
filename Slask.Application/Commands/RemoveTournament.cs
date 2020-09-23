381bc79c1e682b5391d0c74ce9f41baf5efbb822
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-23 09:17:23 +0200

    Implement RemoveTournament command

---
+++ Slask.Application/Commands/RemoveTournament.cs
@@ -0,0 +1,40 @@
+﻿using CSharpFunctionalExtensions;
+using Slask.Application.Commands.Interfaces;
+using Slask.Persistence.Services;
+using System;
+
+namespace Slask.Application.Commands
+{
+    public sealed class RemoveTournament : CommandInterface
+    {
+        public Guid TournamentId { get; }
+
+        public RemoveTournament(Guid tournamentId)
+        {
+            TournamentId = tournamentId;
+        }
+    }
+
+    public sealed class RemoveTournamentHandler : CommandHandlerInterface<RemoveTournament>
+    {
+        private readonly TournamentServiceInterface _tournamentService;
+
+        public RemoveTournamentHandler(TournamentServiceInterface tournamentService)
+        {
+            _tournamentService = tournamentService;
+        }
+
+        public Result Handle(RemoveTournament command)
+        {
+            bool tournamentRemoved = _tournamentService.RemoveTournament(command.TournamentId);
+
+            if (!tournamentRemoved)
+            {
+                return Result.Failure($"Could remove tournament ({ command.TournamentId }). Tournament not found.");
+            }
+
+            _tournamentService.Save();
+            return Result.Success();
+        }
+    }
+}
