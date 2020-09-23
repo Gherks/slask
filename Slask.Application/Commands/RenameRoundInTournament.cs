fee59229f79aeb86b4f0c0f5a8fb9a1e59a4a060
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-09-23 09:32:46 +0200

    Implement RenameRoundInTournament command

--- Slask.Application/Commands/RenameRoundInTournament.cs
+++ Slask.Application/Commands/RenameRoundInTournament.cs
@@ -1,5 +1,7 @@
 ﻿using CSharpFunctionalExtensions;
 using Slask.Application.Commands.Interfaces;
+using Slask.Domain;
+using Slask.Domain.Rounds;
 using Slask.Persistence.Services;
 using System;
 
@@ -8,28 +10,49 @@ namespace Slask.Application.Commands
     public sealed class RenameRoundInTournament : CommandInterface
     {
         public Guid TournamentId { get; }
+        public Guid RoundId { get; }
+        public string NewRoundName { get; }
 
-        public RenameRoundInTournament(Guid tournamentId)
+        public RenameRoundInTournament(Guid tournamentId, Guid roundId, string newRoundName)
         {
             TournamentId = tournamentId;
+            RoundId = roundId;
+            NewRoundName = newRoundName;
         }
     }
 
     public sealed class RenameRoundInTournamentHandler : CommandHandlerInterface<RenameRoundInTournament>
     {
-        private readonly UserServiceInterface _userService;
         private readonly TournamentServiceInterface _tournamentService;
 
-        public RenameRoundInTournamentHandler(
-                UserServiceInterface userService,
-                TournamentServiceInterface tournamentService)
+        public RenameRoundInTournamentHandler(TournamentServiceInterface tournamentService)
         {
-            _userService = userService;
             _tournamentService = tournamentService;
         }
 
         public Result Handle(RenameRoundInTournament command)
         {
+            Tournament tournament = _tournamentService.GetTournamentById(command.TournamentId);
+
+            if (tournament == null)
+            {
+                return Result.Failure($"Could not rename round ({ command.RoundId }) to { command.NewRoundName } in tournament ({ command.TournamentId }). Tournament not found.");
+            }
+
+            RoundBase round = tournament.GetRoundById(command.RoundId);
+
+            if (round == null)
+            {
+                return Result.Failure($"Could not rename round ({ command.RoundId }) to { command.NewRoundName } in tournament ({ command.TournamentId }). Round not found.");
+            }
+
+            bool renameSuccessful = _tournamentService.RenameRoundInTournament(round, command.NewRoundName);
+
+            if (!renameSuccessful)
+            {
+                return Result.Failure($"Could not rename round ({ command.RoundId }) to { command.NewRoundName } in tournament ({ command.TournamentId }).");
+            }
+
             _tournamentService.Save();
             return Result.Success();
         }
