c4d492f598963069460805957ed83a8a173b3960
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-10-12 12:15:24 +0200

    Add API test project

---
+++ Test/API/Slask.API.Specflow.IntegrationTests/UserController.feature
@@ -0,0 +1,9 @@
+﻿Feature: UserController
+	Makes sure all api calls for the user controller are running correctly
+
+@UserControllerTag
+Scenario: Can create a user
+	When API POST is called with address "/api/user" containing body
+		| Username  |
+		| Stålberto |
+	Then the POST result should return status code "200"
---
+++ Test/API/Slask.API.Specflow.IntegrationTests/UserControllerSteps.cs
@@ -0,0 +1,25 @@
+﻿using Slask.SpecFlow.IntegrationTests.PersistenceTests;
+using TechTalk.SpecFlow;
+
+namespace Slask.API.Specflow.IntegrationTests
+{
+    [Binding, Scope(Feature = "UserController")]
+    public class UserControllerSteps : SpecflowCoreSteps
+    {
+        public UserControllerSteps()
+        {
+        }
+
+        [When(@"API POST is called with address ""(.*)"" containing body")]
+        public void WhenAPIPOSTIsCalledWithAddressContainingBody(string p0, Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the POST result should return status code ""(.*)""")]
+        public void ThenThePOSTResultShouldReturnStatusCode(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
