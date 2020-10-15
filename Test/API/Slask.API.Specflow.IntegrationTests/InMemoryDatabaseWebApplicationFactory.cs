5b32a2e29a544caa6ded79805f48a1550fde6946
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-10-15 19:16:40 +0200

    Create a web application factory
    Wil be used when creating clients for testing the API. This factory does the same as the normal API except that it is connected to an in-memory database.

---
+++ Test/API/Slask.API.Specflow.IntegrationTests/InMemoryDatabaseWebApplicationFactory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Slask.Persistence;
+using System;
+
+namespace Slask.API.Specflow.IntegrationTests
+{
+    public class InMemoryDatabaseWebApplicationFactory<StartupType> : WebApplicationFactory<StartupType> where StartupType : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                ServiceProvider serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                services.AddDbContext<SlaskContext>(options =>
+                    {
+                        options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+                        options.UseInternalServiceProvider(serviceProvider);
+                        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                    },
+                    ServiceLifetime.Transient,
+                    ServiceLifetime.Transient);
+
+                ServiceProvider builtServiceProvider = services.BuildServiceProvider();
+
+                using (IServiceScope scope = builtServiceProvider.CreateScope())
+                {
+                    IServiceProvider scopedServiceProvider = scope.ServiceProvider;
+                    SlaskContext slaskContext = scopedServiceProvider.GetRequiredService<SlaskContext>();
+
+                    slaskContext.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
