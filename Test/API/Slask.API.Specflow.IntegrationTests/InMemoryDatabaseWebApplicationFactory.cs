bd5243f2a819e7f8c3e4fce2e65a6493842ffdb0
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-10-18 10:18:06 +0200

    Rework the in-memory test app client factory

--- Test/API/Slask.API.Specflow.IntegrationTests/InMemoryDatabaseWebApplicationFactory.cs
+++ Test/API/Slask.API.Specflow.IntegrationTests/InMemoryDatabaseWebApplicationFactory.cs
@@ -1,26 +1,38 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Slask.Persistence;
 using System;
+using System.Configuration;
+using System.Linq;
 
 namespace Slask.API.Specflow.IntegrationTests
 {
     public class InMemoryDatabaseWebApplicationFactory<StartupType> : WebApplicationFactory<StartupType> where StartupType : class
     {
+        private readonly InMemoryDatabaseRoot _inMemoryDatabaseRoot = new InMemoryDatabaseRoot();
+        private readonly string _testDatabaseName = "InMemoryTestDatabase_" + Guid.NewGuid().ToString();
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.ConfigureServices(services =>
+            builder.ConfigureTestServices(services =>
             {
-                ServiceProvider serviceProvider = new ServiceCollection()
-                    .AddEntityFrameworkInMemoryDatabase()
-                    .BuildServiceProvider();
+                ServiceDescriptor slaskContextDescriptor = services.SingleOrDefault(descriptor =>
+                    descriptor.ServiceType == typeof(DbContextOptions<SlaskContext>));
 
-                services.AddDbContext<SlaskContext>(options =>
+                if(slaskContextDescriptor != null)
+                {
+                    services.Remove(slaskContextDescriptor);
+                }
+
+                services.AddDbContext<SlaskContext>(options => 
                     {
-                        options.UseInMemoryDatabase(Guid.NewGuid().ToString());
-                        options.UseInternalServiceProvider(serviceProvider);
+                        options.UseInMemoryDatabase(_testDatabaseName, _inMemoryDatabaseRoot);
                         options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
                     },
                     ServiceLifetime.Transient,
@@ -32,8 +44,19 @@ namespace Slask.API.Specflow.IntegrationTests
                 {
                     IServiceProvider scopedServiceProvider = scope.ServiceProvider;
                     SlaskContext slaskContext = scopedServiceProvider.GetRequiredService<SlaskContext>();
+                    var logger = scopedServiceProvider.GetRequiredService<ILogger<InMemoryDatabaseWebApplicationFactory<StartupType>>>();
 
                     slaskContext.Database.EnsureCreated();
+
+                    try
+                    {
+                        // Seed database here if ever needed.
+                    }
+                    catch (Exception exception)
+                    {
+                        const string errorMessage = "An error ocurred when seeding the database with test data. Error: {Message}";
+                        logger.LogError(exception, errorMessage, exception.Message);
+                    }
                 }
             });
         }
