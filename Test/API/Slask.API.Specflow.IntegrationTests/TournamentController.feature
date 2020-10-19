790b882ff87234e6a592ca0b9149c0839130188c
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-10-19 22:29:37 +0200

    Create more Specflow API tests for tournament

--- Test/API/Slask.API.Specflow.IntegrationTests/TournamentController.feature
+++ Test/API/Slask.API.Specflow.IntegrationTests/TournamentController.feature
@@ -6,3 +6,16 @@ Scenario: Can create a tournament
 	Given Content-Type is set to "application/json" and Accept is set to "application/json"
 	When POST request is sent to create a tournament named "GSL 2020"
 	Then response should return with status code "201"
+
+Scenario: Can fetch no tournaments with OK response
+	Given Content-Type is set to "application/json" and Accept is set to "application/json"
+	When GET request is sent to fetch tournaments
+	Then response should return with status code "200"
+
+Scenario: Can fetch existing tournaments with OK response
+	Given Content-Type is set to "application/json" and Accept is set to "application/json"
+		And POST request is sent to create a tournament named "Homestory Cup XX"
+		And POST request is sent to create a tournament named "GSL 2019"
+	When GET request is sent to fetch tournaments
+	Then response should return with status code "200"
+		And response should contain tournaments "Homestory Cup XX, GSL 2019"
--- Test/API/Slask.API.Specflow.IntegrationTests/TournamentControllerSteps.cs
+++ Test/API/Slask.API.Specflow.IntegrationTests/TournamentControllerSteps.cs
@@ -1,5 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using FluentAssertions;
+using Newtonsoft.Json;
 using Slask.API.Specflow.IntegrationTests.Utilities;
+using Slask.Common;
+using Slask.Dto;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using TechTalk.SpecFlow;
@@ -9,9 +13,18 @@ namespace Slask.API.Specflow.IntegrationTests
     [Binding, Scope(Feature = "TournamentController")]
     public class TournamentControllerSteps : ControllerStepsBase
     {
+        private const int _acceptableInaccuracy = 2000;
+
         public TournamentControllerSteps(InMemoryDatabaseWebApplicationFactory<Startup> factory)
             : base(factory)
         {
+        }
+
+        [Given(@"GET request is sent to fetch tournaments")]
+        [When(@"GET request is sent to fetch tournaments")]
+        public async Task GivenGETRequestIsSentToFetchTournaments()
+        {
+            _response = await _client.GetAsync("api/tournaments");
         }
 
         [Given(@"POST request is sent to create a tournament named ""(.*)""")]
@@ -22,6 +35,25 @@ namespace Slask.API.Specflow.IntegrationTests
             HttpContent content = CreateHttpContent(jsonContent);
 
             _response = await _client.PostAsync("api/tournaments", content);
+        }
+
+        [Then(@"response should contain tournaments ""(.*)""")]
+        public async Task ThenResponseShouldContainTournaments(string commaSeparatedTournamentNames)
+        {
+            List<string> tournamentNames = commaSeparatedTournamentNames.ToStringList(",");
+
+            List<BareTournamentDto> bareTournamentDtos = await JsonResponseToObjectList<BareTournamentDto>();
+
+            bareTournamentDtos.Should().HaveCount(tournamentNames.Count);
+
+            for (int index = 0; index < bareTournamentDtos.Count; ++index)
+            {
+                BareTournamentDto bareTournamentDto = bareTournamentDtos[index];
+
+                bareTournamentDto.Id.Should().NotBeEmpty();
+                bareTournamentDto.Name.Should().Be(tournamentNames[index]);
+                bareTournamentDto.Created.Should().BeCloseTo(SystemTime.Now, _acceptableInaccuracy);
+            }
         }
     }
 }
