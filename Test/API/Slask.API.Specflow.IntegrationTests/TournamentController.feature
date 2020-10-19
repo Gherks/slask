a5ba930fe3402ac2c84adf762cabd6520492e436
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-10-19 21:14:11 +0200

    Create first tournament API Specflow test

---
+++ Test/API/Slask.API.Specflow.IntegrationTests/TournamentController.feature
@@ -0,0 +1,8 @@
+﻿Feature: TournamentController
+	Makes sure all api calls for the tournament controller are running correctly
+
+@TournamentControllerTag
+Scenario: Can create a tournament
+	Given Content-Type is set to "application/json" and Accept is set to "application/json"
+	When POST request is sent to create a tournament named "GSL 2020"
+	Then response should return with status code "201"
---
+++ Test/API/Slask.API.Specflow.IntegrationTests/TournamentControllerSteps.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Slask.API.Specflow.IntegrationTests.Utilities;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Slask.API.Specflow.IntegrationTests
+{
+    [Binding, Scope(Feature = "TournamentController")]
+    public class TournamentControllerSteps : ControllerStepsBase
+    {
+        public TournamentControllerSteps(InMemoryDatabaseWebApplicationFactory<Startup> factory)
+            : base(factory)
+        {
+        }
+
+        [Given(@"POST request is sent to create a tournament named ""(.*)""")]
+        [When(@"POST request is sent to create a tournament named ""(.*)""")]
+        public async Task GivenPOSTRequestIsSentToCreateATournamentNamed(string name)
+        {
+            string jsonContent = JsonConvert.SerializeObject(new { tournamentName = name });
+            HttpContent content = CreateHttpContent(jsonContent);
+
+            _response = await _client.PostAsync("api/tournaments", content);
+        }
+    }
+}
