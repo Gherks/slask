58156891cc36143c4e7a2fc1394d0b78ef369d7c
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2020-11-26 18:24:37 +0100

    Customize put requests when testing better logic
    Make it possible to add betters to tournament by user id as well as username. Add "-Id" to the end of UserIdentifier value to fetch by user id instead.

--- Test/API/Slask.API.Specflow.IntegrationTests/TournamentController/Better.cs
+++ Test/API/Slask.API.Specflow.IntegrationTests/TournamentController/Better.cs
@@ -1,4 +1,9 @@
 ﻿using Slask.API.Specflow.IntegrationTests.Utilities;
+using Slask.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 
 namespace Slask.API.Specflow.IntegrationTests.TournamentController
@@ -9,6 +14,47 @@ namespace Slask.API.Specflow.IntegrationTests.TournamentController
         public Better(InMemoryDatabaseWebApplicationFactory<Startup> factory)
             : base(factory)
         {
+
+        }
+
+        public override async Task GivenPUTRequestIsSentTo(string uri, Table table)
+        {
+            const string userIdentifier = "UserIdentifier";
+            const string idReplacementTag = "-Id";
+
+            foreach (TableRow row in table.Rows)
+            {
+                bool containsUserIdentifierKey = row.Keys.Contains(userIdentifier);
+
+                if (containsUserIdentifierKey)
+                {
+                    string identifier = row[userIdentifier];
+                    string identifierTag = identifier.Substring(identifier.Length - idReplacementTag.Length);
+
+                    bool shouldReplaceUsernameWithUserId = identifierTag == idReplacementTag;
+
+                    if (shouldReplaceUsernameWithUserId)
+                    {
+                        Guid userId = await ReplaceUserIdentifierNameWithId(identifier, row);
+
+                        row[userIdentifier] = userId.ToString();
+                    }
+                }
+            }
+
+            await base.GivenPUTRequestIsSentTo(uri, table);
+        }
+
+        private async Task<Guid> ReplaceUserIdentifierNameWithId(string identifier, TableRow row)
+        {
+            string username = identifier.Substring(0, identifier.Length - 3);
+
+            await GivenGETRequestIsSentTo("api/users/" + username);
+            string responseContent = await _response.Content.ReadAsStringAsync();
+
+            List<UserDto> userDtos = JsonResponseToObjectList<UserDto>(responseContent);
+
+            return userDtos.First().Id;
         }
     }
 }
